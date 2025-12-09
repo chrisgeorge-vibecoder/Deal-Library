8f072832ae6e872677ba63c664c569f0acbb038a
Sarat Jogavajjala <sharatvikas@gmail.com>
2025-12-09 13:58:29 -0700

    Fix

--- deal-library-amplify-app/next.config.js
+++ deal-library-amplify-app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   
   // Skip type checking during build for faster deployments
   typescript: {
